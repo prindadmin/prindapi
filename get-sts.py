659a407cb832f4ac74d949ee22cd86585748e84c
Simon <mr.simon.hunt@gmail.com>
2020-01-21 11:14:34 +0000

    added cognito authorizer and permissions for assume-role and get-sts is now returning credentials

--- get-sts.py
+++ get-sts.py
@@ -17,63 +17,63 @@ def get_cognito_username(token):
 
 def lambda_handler(event, context):
 
-    try:
+    # try:
         
-        # jwt_token = event['headers']['Authorization']
-        # cognito_username = get_cognito_username(jwt_token)
-        # s3_bucket_arn = os.environ['S3_BUCKET_ARN']
+    # "cognitoPoolClaims": {
+    #     "sub": "81de030e-7a68-4426-9c12-160fca975a92"
+    # },
 
-        # #Connect to the STS system
-        # client = boto3.client('sts')
+    jwt_token = event['headers']['Authorization']
+    cognito_username = event['cognitoPoolClaims']['sub']
+    s3_bucket_arn = os.environ['S3_BUCKET_ARN']
 
-        print(event)
+    # #Connect to the STS system
+    client = boto3.client('sts')
 
-
-
-        #Create the policy to allow users to add files to their s3 bucket
-        # policy = json.dumps({
-        #     "Version": "2012-10-17",
-        #     "Statement": [
-        #         {
-        #             "Sid" : "VisualEditor0",
-        #             "Effect": "Allow",
-        #             "Action": [
-        #                 "s3:PutObject",
-        #                 "s3:GetObject"
-        #             ],
-        #             "Resource": ["{s3_bucket_arn}/{cognito_username}/*",
-        #                          "{s3_bucket_arn}/demonstration_sites/*"]
-        #         }
-        #     ]
-        # })
-        
-        # #Get tokens for user to assume the role
-        # response = client.assume_role(
-        #     RoleArn = "arn:aws:iam::514296467270:role/webClientRole",
-        #     Policy = policy,
-        #     RoleSessionName = "webClientRole",
-        #     DurationSeconds = 3600
-        # )
+    #Create the policy to allow users to add files to their s3 bucket
+    policy = json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid" : "VisualEditor0",
+                "Effect": "Allow",
+                "Action": [
+                    "s3:PutObject",
+                    "s3:GetObject"
+                ],
+                "Resource": [f"{s3_bucket_arn}/{cognito_username}/*",
+                            f"{s3_bucket_arn}/demonstration_sites/*"]
+            }
+        ]
+    })
+    
+    #Get tokens for user to assume the role
+    response = client.assume_role(
+        RoleArn = "arn:aws:iam::434494845257:role/webClientRole",
+        Policy = policy,
+        RoleSessionName = "webClientRole",
+        DurationSeconds = 3600
+    )
           
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+    #     print(e)
         
-        return {"Error" : "Internal server error when getting STS Token"}, 500
+    #     return {"Error" : "Internal server error when getting STS Token"}, 500
 
-
+    # return {
+    #     "cognitoUsername": cognito_username,
+    #     "s3BucketArn": s3_bucket_arn
+    # }
     
     return {
-        "event": event
+        "statusCode": 200,
+        "body": {
+            "SessionToken" : response["Credentials"]["SessionToken"],
+            "Expiration" : response["Credentials"]["Expiration"].timestamp(),
+            "AccessKeyId" : response["Credentials"]["AccessKeyId"], 
+            "SecretAccessKey" : response["Credentials"]["SecretAccessKey"]
+        }
     }
-    # return {
-    #     "statusCode": 200,
-    #     "body": {
-    #         "SessionToken" : response["Credentials"]["SessionToken"],
-    #         "Expiration" : response["Credentials"]["Expiration"].timestamp(),
-    #         "AccessKeyId" : response["Credentials"]["AccessKeyId"], 
-    #         "SecretAccessKey" : response["Credentials"]["SecretAccessKey"]
-    #     }
-    # }
         
 
         
