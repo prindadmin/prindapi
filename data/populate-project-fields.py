0f6032e697221b6105e8bc9d3af4308c72ca4c2d
Simon <mr.simon.hunt@gmail.com>
2020-03-02 14:16:48 +0000

    added prompt for 'stage' in populate field and populate roles scripts

--- data/populate-project-fields.py
+++ data/populate-project-fields.py
@@ -2,8 +2,10 @@ import boto3
 import json
 import os
 
+stage = input("enter stage: ")
+
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('prind-dev')
+table = dynamodb.Table(f'prind-{stage}')
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
