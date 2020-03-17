a02709411ec6efdab39a4c2e1514cf123915ee9f
Simon <mr.simon.hunt@gmail.com>
2020-03-17 15:43:38 +0000

    added a forgotPassword email to the custom-message lambda

--- custom-message.js
+++ custom-message.js
@@ -18,10 +18,21 @@ exports.handler = async (event, context) => {
       <a href="https://${portalHostname}.prind.tech/#/confirm-email?client_id=${event.callerContext.clientId}&user_name=${event.userName}&confirmation_code=${event.request.codeParameter}">this link</a>
     `;
 
+    var forgotPasswordMessage = `
+      Please reset your password by clicking 
+      <a href="https://${portalHostname}.prind.tech/#/reset-password?client_id=${event.callerContext.clientId}&user_name=${event.userName}&confirmation_code=${event.request.codeParameter}">this link</a>
+    `;
+
     if (event.triggerSource === "CustomMessage_SignUp") {
       event.response.emailSubject = "Please verify your email address";
       event.response.emailMessage = confirmEmailMessage;
       event.response.smsMessage = "Welcome to the service. Your confirmation code is " + event.request.codeParameter;
+    }
+
+    else if (event.triggerSource === "CustomMessage_ForgotPassword") {
+        event.response.emailSubject = "Your password reset";
+        event.response.emailMessage = forgotPasswordMessage;
+        event.response.smsMessage = "Welcome to the service. Your confirmation code is " + event.request.codeParameter;
     }
 
     console.log('Returned event:', JSON.stringify(event, null, 2));
